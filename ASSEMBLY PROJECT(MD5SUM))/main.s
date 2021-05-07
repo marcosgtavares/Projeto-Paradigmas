84db13b14b22b4cac88b5bdffb175f952f304d2a
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-05-07 10:35:50 -0400

    padding done

--- ASSEMBLY PROJECT(MD5SUM))/main.s
+++ ASSEMBLY PROJECT(MD5SUM))/main.s
@@ -1,7 +1,9 @@
 SECTION .data
-firstpad        db 80h
-npad            db 0
-bycount         dd 0
+h0              db 67452301h
+h1              db EFCDAB89h
+h2              db 98BADCFEh
+h3              db 10325476h
+tlenght         dd 0    
 
 SECTION .bss
 fileallocmem:   resb 2000000
@@ -12,10 +14,10 @@ global _start
 
 _start:
     mov eax, 5                      ;open file 
-    mov ebx, [esp + 4]
+    mov ebx, [esp + 8]
     mov ecx, 0
     mov edx, 0644o
-    INT 80h
+    int 80h
 
 
     mov ebx, eax                    ;read 512bit from the file if possible
@@ -28,6 +30,8 @@ _start:
 
     mov esi, 0
     mov [flenght], esi
+
+    add [tlenght], 1                ;add lenght in 64 bytes
 
     add [flenght], eax              ;add lenght in bits
     add [flenght], eax
@@ -43,9 +47,12 @@ _start:
     jg padding                      ;if read bytes are less than 64, go to padding
 
 readalloc:                          ;read on allocated memory until the end                         
-    syscall
+    mov eax, 3
+    int 80h
     
     add ecx, 64                     ;move 64 bytes on the allocated memory
+
+    add [tlenght], 1                ;add lenght in 64 bytes
 
     add [flenght], eax              ;add lenght in bits
     add [flenght], eax
@@ -56,49 +63,43 @@ readalloc:                          ;read on allocated memory until the end
     add [flenght], eax
     add [flenght], eax
 
-    
-    cmp esi, eax                     ;if there is no more to read
-    je complete3
     cmp edi, eax                     ;if read bytes are less than 64, go to padding
     jg padding
     jmp readalloc                    ;otherwise keep looping
 
 padding:
-    sub ecx, 63
     mov edi, 56
-    cmp edi, eax                     ;if read bytes are less than 56, go to complete(to add bytes until it's properly padded)
-    jg complete
+    cmp edi, eax                     
+    jg complete                     ;if read bytes are less than 56, go to complete(to add bytes until it's properly padded)
     je complete2                    ;otherwise add 64 bytes(first byte 10000000)
-    jl complete4                    ;otherwise add to 64 and add 56
+    jl complete3                    ;otherwise add to 64 and add 56
 
 complete:                           ;complete 56
-    mov [ecx], byte 80h
-    mov edi, 56
+    mov edi, 55
     sub edi, eax
-    sub edi, 1
+    sub ecx, edi
+    mov [ecx], byte 4ah
     jmp looppad
 
 complete2:                          ;add 64
-    mov [ecx], byte 80h
+    add [tlenght], 1                ;add lenght in 64 bytes
+    sub ecx, 7
+    mov [ecx], byte 4ah
     mov edi, 63
     jmp looppad
 
-complete3:                          ;add 56
-    sub ecx, 63
-    mov [ecx], byte 80h
-    mov edi, 55
-    jmp looppad
-
-complete4:                          ;otherwise add to 64 and add 56
-    mov [ecx], byte 80h
+complete3:                          ;otherwise add to 64 and add 56
+    add [tlenght], 1                ;add lenght in 64 bytes
     mov edi, 64
-    sub edi, eax
+    sub edi, eax                    
+    sub ecx, edi
+    mov [ecx], byte 4ah
     add edi, 55
 
 looppad:                            ;add fisrt padding and loop for the rest
     jz addbitlenght
     add ecx, 1
-    mov [ecx], byte 0
+    mov [ecx], byte 4bh
     sub edi, 1
     jmp looppad
 
@@ -106,16 +107,22 @@ addbitlenght:
     add ecx, 1
     mov ebx, dword[flenght]
     mov [ecx], ebx
-    add ecx, 4
+    add ecx, 1
     mov ebx, dword[flenght+4]
     mov [ecx], ebx
+
+foreachchunk64:
+    
+    sub [tlenght], 1
+    jz foreachchunk64
 
 print:
     mov eax, 4
     mov ebx, 1
     mov ecx, fileallocmem
-    mov edx, 100
-    syscall
+    mov edx, 1000000
+    int 80h
+   
     mov eax, 1
     mov ebx, 0
-    syscall
+    int 80h
