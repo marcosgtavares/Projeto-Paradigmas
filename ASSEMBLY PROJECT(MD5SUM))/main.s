195001c3a15dde0586db77326bd2b3fe57f2f865
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-05-07 12:17:46 -0400

    loop and main functions prototipe

--- ASSEMBLY PROJECT(MD5SUM))/main.s
+++ ASSEMBLY PROJECT(MD5SUM))/main.s
@@ -1,18 +1,29 @@
 SECTION .data
-h0              db 67452301h
-h1              db EFCDAB89h
-h2              db 98BADCFEh
-h3              db 10325476h
-tlenght         dd 0    
-
 SECTION .bss
 fileallocmem:   resb 2000000
 flenght:        resb 8
+inita:          resb 4
+initb:          resb 4
+initc:          resb 4
+initd:          resb 4
+h0:             resb 4 
+h1:             resb 4 
+h2:             resb 4 
+h3:             resb 4 
+tlenght:        resb 4    
+nofww:          resb 4 
 
 SECTION .text
 global _start
 
 _start:
+    mov dword [h0], 67452301h
+    mov dword [h1], 4023233417
+    mov dword [h2], 98badcfeh
+    mov dword [h3], 10325476h
+    mov dword [tlenght], 0
+    mov dword [nofww], 16
+
     mov eax, 5                      ;open file 
     mov ebx, [esp + 8]
     mov ecx, 0
@@ -31,7 +42,7 @@ _start:
     mov esi, 0
     mov [flenght], esi
 
-    add [tlenght], 1                ;add lenght in 64 bytes
+    add dword [tlenght], 1                ;add lenght in 64 bytes
 
     add [flenght], eax              ;add lenght in bits
     add [flenght], eax
@@ -52,7 +63,7 @@ readalloc:                          ;read on allocated memory until the end
     
     add ecx, 64                     ;move 64 bytes on the allocated memory
 
-    add [tlenght], 1                ;add lenght in 64 bytes
+    add dword [tlenght], 1                ;add lenght in 64 bytes
 
     add [flenght], eax              ;add lenght in bits
     add [flenght], eax
@@ -82,14 +93,14 @@ complete:                           ;complete 56
     jmp looppad
 
 complete2:                          ;add 64
-    add [tlenght], 1                ;add lenght in 64 bytes
+    add dword[tlenght], 1                ;add lenght in 64 bytes
     sub ecx, 7
     mov [ecx], byte 4ah
     mov edi, 63
     jmp looppad
 
 complete3:                          ;otherwise add to 64 and add 56
-    add [tlenght], 1                ;add lenght in 64 bytes
+    add dword [tlenght], 1                ;add lenght in 64 bytes
     mov edi, 64
     sub edi, eax                    
     sub ecx, edi
@@ -111,10 +122,18 @@ addbitlenght:
     mov ebx, dword[flenght+4]
     mov [ecx], ebx
 
-foreachchunk64:
-    
-    sub [tlenght], 1
+foreachchunk64:                     ;main loop for each 64 bytes
+foreach4bytes:                      ;loop for each 4 bytes
+
+
+    sub dword [nofww], 1
+    jz foreach4bytes
+
+    add dword [nofww], 16
+    sub dword [tlenght], 1
     jz foreachchunk64
+
+appendhn:
 
 print:
     mov eax, 4
@@ -126,3 +145,52 @@ print:
     mov eax, 1
     mov ebx, 0
     int 80h
+
+ff:
+    mov ebx, b
+    and ebx, c   
+    mov eax, 4294967295
+    xor eax, b
+    and eax, d
+    or eax, ebx
+    
+    add a, eax
+    add a, x
+    add a, s
+    add a, ac
+
+gg:
+    mov ebx, b
+    and ebx, d   
+    mov eax, 4294967295
+    xor eax, d
+    and eax, c
+    or eax, ebx
+
+    add a, eax
+    add a, x
+    add a, s
+    add a, ac
+
+hh:
+    mov ebx, b
+    xor ebx, c
+    xor ebx, d
+
+    add a, eax
+    add a, x
+    add a, s
+    add a, ac
+
+ii:
+    mov eax, c
+    mov ebx, b
+    mov ecx, 4294967295
+    xor ecx, d
+    or ebx, ecx
+    xor eax, ebx
+
+    add a, eax
+    add a, x
+    add a, s
+    add a, ac
