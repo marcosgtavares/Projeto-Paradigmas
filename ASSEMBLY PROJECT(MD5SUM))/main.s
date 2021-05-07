c813b835194069a27260207fed7864a364fa510d
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-05-07 15:39:45 -0400

    functions prototypes developed

--- ASSEMBLY PROJECT(MD5SUM))/main.s
+++ ASSEMBLY PROJECT(MD5SUM))/main.s
@@ -9,20 +9,25 @@ initd:          resb 4
 h0:             resb 4 
 h1:             resb 4 
 h2:             resb 4 
-h3:             resb 4 
+h3:             resb 4
+a:              resb 4 
+b:              resb 4 
+c:              resb 4 
+d:              resb 4
+x:              resb 4
+s:              resb 4
+ac:             resb 4 
 tlenght:        resb 4    
-nofww:          resb 4 
 
 SECTION .text
 global _start
 
 _start:
-    mov dword [h0], 67452301h
-    mov dword [h1], 4023233417
-    mov dword [h2], 98badcfeh
-    mov dword [h3], 10325476h
+    mov dword [h3], 1732584193
+    mov dword [h2], 4023233417
+    mov dword [h1], 2562383102
+    mov dword [h0], 271733878
     mov dword [tlenght], 0
-    mov dword [nofww], 16
 
     mov eax, 5                      ;open file 
     mov ebx, [esp + 8]
@@ -42,7 +47,7 @@ _start:
     mov esi, 0
     mov [flenght], esi
 
-    add dword [tlenght], 1                ;add lenght in 64 bytes
+    add dword [tlenght], 1          ;add lenght in 64 bytes
 
     add [flenght], eax              ;add lenght in bits
     add [flenght], eax
@@ -63,7 +68,7 @@ readalloc:                          ;read on allocated memory until the end
     
     add ecx, 64                     ;move 64 bytes on the allocated memory
 
-    add dword [tlenght], 1                ;add lenght in 64 bytes
+    add dword [tlenght], 1          ;add lenght in 64 bytes
 
     add [flenght], eax              ;add lenght in bits
     add [flenght], eax
@@ -93,14 +98,14 @@ complete:                           ;complete 56
     jmp looppad
 
 complete2:                          ;add 64
-    add dword[tlenght], 1                ;add lenght in 64 bytes
+    add dword[tlenght], 1           ;add lenght in 64 bytes
     sub ecx, 7
     mov [ecx], byte 4ah
     mov edi, 63
     jmp looppad
 
 complete3:                          ;otherwise add to 64 and add 56
-    add dword [tlenght], 1                ;add lenght in 64 bytes
+    add dword [tlenght], 1          ;add lenght in 64 bytes
     mov edi, 64
     sub edi, eax                    
     sub ecx, edi
@@ -122,15 +127,37 @@ addbitlenght:
     mov ebx, dword[flenght+4]
     mov [ecx], ebx
 
+    mov edx, fileallocmem
+
 foreachchunk64:                     ;main loop for each 64 bytes
-foreach4bytes:                      ;loop for each 4 bytes
+    mov edi, inita
+    mov esi, dword [h0]
+    mov [edi], esi
+    mov edi, initb
+    mov esi, dword [h1]
+    mov [edi], esi
+    mov edi, initc
+    mov esi, dword [h2]
+    mov [edi], esi
+    mov edi, initd
+    mov esi, dword [h3]
+    mov [edi], esi
 
+    push dword [h0]
+    push dword [h1]
+    push dword [h2]
+    push dword [h3]
+    push dword [edx]
+    push dword 7
+    push dword 3614090360
+    ;call ff
+    pop dword[h3]
+    pop dword[h2]
+    pop dword[h1]
+    pop dword[h0]
 
-    sub dword [nofww], 1
-    jz foreach4bytes
-
-    add dword [nofww], 16
     sub dword [tlenght], 1
+    add edx, 64
     jz foreachchunk64
 
 appendhn:
@@ -147,50 +174,150 @@ print:
     int 80h
 
 ff:
-    mov ebx, b
-    and ebx, c   
-    mov eax, 4294967295
-    xor eax, b
-    and eax, d
+    pop dword [ac]
+    pop dword [s]
+    pop dword [x]
+    pop dword [d]
+    pop dword [c]
+    pop dword [b]
+    pop dword [a]
+
+    mov ebx, dword [b]
+    and ebx, dword [c]   
+    not dword [b]
+    mov eax, dword [b]
+    and eax, dword [d]
     or eax, ebx
     
-    add a, eax
-    add a, x
-    add a, s
-    add a, ac
+    add dword [a], eax
+    mov ebx, dword [x]
+    add dword [a], ebx
+    mov ebx, dword[s]
+    add dword [a], ebx
+    mov ebx, dword[ac]
+    add dword [a], ebx
+
+    mov ebx, dword[s]
+    mov eax, dword[a]
+rotateleftf:    
+    rol eax, 1                      ;segfault
+    sub ebx, 1
+    jz rotateleftf
+
+    add eax, dword[b] 
+
+    push eax
+    push dword [b]
+    push dword [c]
+    push dword [d]
+
+    ret
 
 gg:
-    mov ebx, b
-    and ebx, d   
-    mov eax, 4294967295
-    xor eax, d
-    and eax, c
+    pop dword [ac]
+    pop dword [s]
+    pop dword [x]
+    pop dword [d]
+    pop dword [c]
+    pop dword [b]
+    pop dword [a]
+
+    mov ebx, dword [b]
+    and ebx, dword [d]   
+    not dword [d]
+    mov eax, dword [d]
+    and eax, dword [c]
     or eax, ebx
 
-    add a, eax
-    add a, x
-    add a, s
-    add a, ac
+    add dword [a], eax
+    mov ebx, dword [x]
+    add dword [a], ebx
+    mov ebx, dword[s]
+    add dword [a], ebx
+    mov ebx, dword[ac]
+    add dword [a], ebx
+
+    mov ebx, dword[s]
+    mov eax, dword[a]
+rotateleftg:    
+    rol eax, 1
+    sub ebx, 1
+    jz rotateleftg
+
+    add eax, dword[b] 
+
+    push eax
+    push dword [b]
+    push dword [c]
+    push dword [d]
+
+    ret
 
 hh:
-    mov ebx, b
-    xor ebx, c
-    xor ebx, d
+    pop dword [ac]
+    pop dword [s]
+    pop dword [x]
+    pop dword [d]
+    pop dword [c]
+    pop dword [b]
+    pop dword [a]
 
-    add a, eax
-    add a, x
-    add a, s
-    add a, ac
+    mov ebx, dword [b]
+    xor ebx, dword [c]
+    xor ebx, dword [d]
+
+    add dword [a], eax
+    mov ebx, dword [x]
+    add dword [a], ebx
+    mov ebx, dword[s]
+    add dword [a], ebx
+    mov ebx, dword[ac]
+    add dword [a], ebx
+
+    mov ebx, dword[s]
+    mov eax, dword[a]
+rotatelefth:    
+    rol eax, 1
+    sub ebx, 1
+    jz rotatelefth
+
+    add eax, dword[b] 
+
+    push eax
+    push dword [b]
+    push dword [c]
+    push dword [d]
+
+    ret
 
 ii:
-    mov eax, c
-    mov ebx, b
-    mov ecx, 4294967295
-    xor ecx, d
+    mov eax, dword [c]
+    mov ebx, dword [b]
+    not dword [d]
+    mov ecx, dword [d]
     or ebx, ecx
     xor eax, ebx
 
-    add a, eax
-    add a, x
-    add a, s
-    add a, ac
+    add dword [a], eax
+    mov ebx, dword [x]
+    add dword [a], ebx
+    mov ebx, dword[s]
+    add dword [a], ebx
+    mov ebx, dword[ac]
+    add dword [a], ebx
+
+    mov ebx, dword[s]
+    mov eax, dword[a]
+rotatelefti:    
+    rol eax, 1
+    sub ebx, 1
+    jz rotatelefti
+
+    add eax, dword[b] 
+
+    push eax
+    push dword [b]
+    push dword [c]
+    push dword [d]
+
+    ret
